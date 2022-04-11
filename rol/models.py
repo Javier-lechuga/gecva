e63af8d2086bf8f9887d032399e940ef8144f685
Javier-lechuga <jlechuga@solusoft.com.mx>
2022-04-11 09:46:28 -0500

    fixture para rol y unidad

--- rol/models.py
+++ rol/models.py
@@ -11,3 +11,16 @@ class Rol(models.Model):
 
     def __str__(self):
             return self.nombre
+
+# creación de los roles para los tipos de usuarios que manejara el sistema
+# para crear estos modelos en la base de datos utilizar la fixture rol.json ejecutando
+# el comando "python manage.py loaddata rol.json" en la terminal
+#
+#   roles:
+#       Administrador   - Usuario que administrara el sistema.
+#       Usuario  - Usuario que podra crear expedientes.
+#       Aprobación  - Usuario que podra aprobar expedientes y documentos 
+
+#
+# Ingreso de los estatus anteriores al modelo Estatus
+#  
